63049893e8250e002b1736711ff84472e63714ff
Suaste Cherizola <jsuaste86@gmail.com>
2024-07-01 12:15:19 +0200

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import './App.css'
+import hello from './assets/images/cover5.jpeg'
+
+function App() {
+
+  return (
+    <>
+      <div>
+        Cool website on the making
+      </div>
+      <img src={hello}></img>
+    
+    </>
+  )
+}
+
+export default App
